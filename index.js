9aca79966df980511ab7358277586d109185bab0
Sumit Sinha <sumitsinha1989@gmail.com>
2015-11-23 18:13:20 +0530

    Works. But it writes multiple time. Need to check that

--- index.js
+++ index.js
@@ -5,40 +5,48 @@ var fs = require('fs')
 var yamlfilename = 'destination.yml'
 var yamlfilepath = '' + yamlfilename;
 
-var rl = require('readline').createInterface({
-    terminal: false,
-    input: require('fs').createReadStream(yamlfilepath)
-});
 
-rl.on('line', function (ymlline) {
-    //    console.log('Line from file:', line);
-    csv
-        .fromPath("source.csv")
-        .on("data", function (transline) {
-            var ymlline_split = ymlline.split("'");
-            //console.log(ymlline_split[1]);
-            //  console.log("checking "+"("+ymlline_split[1]+" === "+transline[0]+")");
-            if (ymlline_split[1] === transline[0]) {
-                console.log("Match found!");
-                //console.log(data[0]);
+fs.readFile(yamlfilepath, 'utf8', function (err, dataa) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = dataa;
 
-                fs.readFile(yamlfilepath, 'utf8', function (err, dataa) {
-                    if (err) {
-                        return console.log(err);
+    var rl = require('readline').createInterface({
+        terminal: false,
+        input: require('fs').createReadStream(yamlfilepath)
+    });
+
+    rl.on('line', function (ymlline) {
+            console.log('Line from file:', ymlline);
+            csv
+                .fromPath("source.csv")
+                .on("data", function (transline) {
+                    var ymlline_split = ymlline.split("'");
+                    //console.log(ymlline_split[1]);
+                    //  console.log("checking "+"("+ymlline_split[1]+" === "+transline[0]+")");
+                    if (ymlline_split[1] === transline[0]) {
+                        console.log("Match found!");
+                        //console.log(data[0]);
+
+                        result = result.replace(RegExp(ymlline_split[1], 'g'), transline[1]);
+                        console.log(RegExp(ymlline_split[1], 'g') + " " + transline[1]);
+                        //console.log(result);
+
                     }
-                    var result = dataa.replace(RegExp(ymlline_split[1], 'g'), transline[1]);
-                    console.log(RegExp(ymlline_split[1], 'g') + " " + transline[1]);
-                    //console.log(result);
-
+                })
+                .on("end", function () {
+                    //console.log("done");
                     fs.writeFile(yamlfilepath, result, 'utf8', function (err) {
-                        console.log("I wrote this on file "+result);
+                        console.log("I wrote this on file " + result);
                         if (err) return console.log(err);
                     });
                 });
-            }
-
         })
-        .on("end", function () {
-            //console.log("done");
-        });
+//        .on('close', function () {
+//            fs.writeFile(yamlfilepath, result, 'utf8', function (err) {
+//                console.log("I wrote this on file " + result);
+//                if (err) return console.log(err);
+//            });
+//        });
 });
